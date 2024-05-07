f994857640f7de388d1ce939639c7ecd825365c7
Neal Powers <neal.grindstaff@gmail.com>
2024-05-07 15:38:36 -0400

    added lots

--- helpers.php
+++ helpers.php
@@ -7,6 +7,67 @@
  * @return string
  */
 
- function basePath($path = ''){
-    return __DIR__ .'/'. $path;
- }
+function basePath($path = '')
+{
+    return __DIR__ . DIRECTORY_SEPARATOR . $path;
+}
+
+
+/**
+ * Load a view
+ * @param string $name
+ * @return void
+ */
+
+function loadView($name, $data)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+
+    if (file_exists($viewPath)) {
+        extract($data);
+        require $viewPath;
+    } else {
+        echo "View '{$name} not found'";
+    }
+}
+
+/**
+ * Load a partial
+ * @param string $name
+ * @return void
+ */
+function loadPartial($name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "Partial '{$name} not found'";
+    }
+}
+
+/**
+ * Inspect a value(s) and die
+ * 
+ * @param mixed $value;
+ * @return void
+ */
+
+function inspectAndDie($value)
+{
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
+}
+
+/**
+ * Format Salary
+ * 
+ * @param string $salary
+ * @return string Formatted Salary
+ */
+function formatSalary($salary)
+{
+    return '$' . number_format(floatval($salary));
+}
